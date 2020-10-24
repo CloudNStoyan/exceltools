daf8f32af4a5dbd9eb6c689019e8881a83292bb4
Stoyan <slownessbg@gmail.com>
2020-10-24 23:58:50 +0300

    adding ui for the new feature count by color

---
+++ ExcelTools/Pages/CountByColor.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+
+namespace ExcelTools.Pages
+{
+    public partial class CountByColor : Page
+    {
+        private Logger Logger { get; }
+        private ExcelAnalysis ExcelAnalysis { get; }
+        public CountByColor(Logger logger)
+        {
+            this.InitializeComponent();
+
+            this.Logger = logger;
+
+            this.ExcelAnalysis = new ExcelAnalysis(this.Logger);
+        }
+
+        private void SelectFileHandler(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog { Filter = "Excel Files|*.xls;*.xlsx|CSV files (*.csv)|*.csv" };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                this.FilePathTextBox.Text = openFileDialog.FileName;
+                this.SelectFileButton.Visibility = Visibility.Hidden;
+
+                this.FilePathViewWrapper.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void ChangeFileHandler(object sender, RoutedEventArgs e)
+        {
+            this.FilePathTextBox.Clear();
+
+            this.FilePathViewWrapper.Visibility = Visibility.Hidden;
+            this.SelectFileButton.Visibility = Visibility.Visible;
+        }
+
+        private void RunAnalysis(object sender, RoutedEventArgs e)
+        {
+            var excelWrapper = new ExcelWrapper(this.FilePathTextBox.Text);
+        }
+    }
+}
