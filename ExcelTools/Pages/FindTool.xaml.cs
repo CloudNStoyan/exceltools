5e24c77c71acd49da58a970451bf180f881fa53f
Stoyan <slownessbg@gmail.com>
2020-10-31 23:58:47 +0200

    new feature find tool

---
+++ ExcelTools/Pages/FindTool.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+
+namespace ExcelTools.Pages
+{
+    public partial class FindTool : Page
+    {
+        private Logger Logger { get; }
+        private ExcelAnalysis ExcelAnalysis { get; }
+        public FindTool(Logger logger)
+        {
+            this.InitializeComponent();
+
+            this.Logger = logger;
+
+            this.ExcelAnalysis = new ExcelAnalysis(this.Logger);
+        }
+
+        private void SelectFileHandler(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog { Filter = "Excel Files|*.xls;*.xlsx|CSV files (*.csv)|*.csv" };
+
+            if (this.MultipleFiles.IsChecked == true)
+            {
+                openFileDialog.Multiselect = true;
+            }
+
+            if (openFileDialog.ShowDialog() != true)
+            {
+                return;
+            }
+
+            this.FilePathTextBox.Text = this.MultipleFiles.IsChecked == true
+                ? string.Join(",", openFileDialog.FileNames.Select(Path.GetFileName))
+                : openFileDialog.FileName;
+
+            this.SelectFileButton.Visibility = Visibility.Hidden;
+
+            this.FilePathViewWrapper.Visibility = Visibility.Visible;
+        }
+
+        private void ChangeFileHandler(object sender, RoutedEventArgs e)
+        {
+            this.FilePathTextBox.Clear();
+
+            this.FilePathViewWrapper.Visibility = Visibility.Hidden;
+            this.SelectFileButton.Visibility = Visibility.Visible;
+        }
+
+        private void RunAnalysis(object sender, RoutedEventArgs e)
+        {
+            if (this.MultipleFiles.IsChecked == false)
+            {
+
+                var excelWrapper = new ExcelWrapper(this.FilePathTextBox.Text);
+                //this.ExcelAnalysis.FindDuplicates(excelWrapper, this.ColumnTextBox.Text);
+            }
+        }
+    }
+}
