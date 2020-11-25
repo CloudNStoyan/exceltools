0107ac234840f94ec9ac8033ac2d1da918838087
Stoyan <slownessbg@gmail.com>
2020-11-25 23:01:50 +0200

    adding new usercontrol and setting up new feature to compare excel files

---
+++ ExcelTools/Pages/CompareTool.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ExcelTools.Attributes;
+
+namespace ExcelTools.Pages
+{
+    [PageInfo(Header = "Compare Excels")]
+    public partial class CompareTool : Page
+    {
+        private Logger Logger { get; }
+        public CompareTool(Logger logger)
+        {
+            this.InitializeComponent();
+
+            this.Logger = logger;
+        }
+
+        private void OnFileSelected()
+        {
+            this.CompareButton.IsEnabled = true;
+        }
+
+        private void OnFileChanged()
+        {
+            this.CompareButton.IsEnabled = false;
+        }
+
+        private void RunAnalysis(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
