51c3bf019234656a6706d5244e377b58838dc9f9
Stoyan <slownessbg@gmail.com>
2020-11-28 23:51:20 +0200

    doing the logic of comparetool

--- ExcelTools/Pages/CompareTool.xaml.cs
+++ ExcelTools/Pages/CompareTool.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using ExcelTools.Attributes;
@@ -17,32 +16,16 @@ namespace ExcelTools.Pages
             this.Logger = logger;
         }
 
-        private void OnFileSelected()
-        {
-            this.CompareButton.IsEnabled = true;
-        }
-
-        private void OnFileChanged()
-        {
-            this.CompareButton.IsEnabled = false;
-        }
-
         private void RunAnalysis(object sender, RoutedEventArgs e)
         {
-            string[] filePaths = {""};
-
-            if (filePaths.Length < 2)
-            {
-                return;
-            }
-
-            var excelWrappers = filePaths.Select(x => new ExcelWrapper(x)).ToArray();
+            var firstExcelWrapper = new ExcelWrapper(this.FirstFileSelection.SelectedFile);
+            var secondExcelWrapper = new ExcelWrapper(this.SecondFileSelection.SelectedFile);
 
             string columnText = this.ColumnTextBox.Text;
             int column = ExcelAnalysis.ConvertStringColumnToNumber(columnText);
 
-            string[] firstExcelRows = excelWrappers[0].GetStringRows(column);
-            string[] secondExcelRows = excelWrappers[1].GetStringRows(column);
+            string[] firstExcelRows = firstExcelWrapper.GetStringRows(column);
+            string[] secondExcelRows = secondExcelWrapper.GetStringRows(column);
 
             foreach (string rowValue in firstExcelRows)
             {
