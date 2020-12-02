e55dd61d0ab9cbfcc785dfc86563b707e384d06f
Stoyan <slownessbg@gmail.com>
2020-12-02 23:59:21 +0200

    adding new control

---
+++ ExcelTools/Controls/Input.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ExcelTools.Controls
+{
+    public partial class Input : UserControl
+    {
+        public Input() => this.InitializeComponent();
+
+        private void MainTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string text = this.MainTextBox.Text;
+
+            this.MainTextBox.Text = text.Length > 1 ? text[text.Length - 1].ToString().ToUpper() : text.ToUpper();
+
+            this.MainTextBox.CaretIndex = 1;
+        }
+    }
+}
