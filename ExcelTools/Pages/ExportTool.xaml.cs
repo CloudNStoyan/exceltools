1d6779d4eb1141b6ef6de23ce4470298b8d74dac
Stoyan <slownessbg@gmail.com>
2020-12-05 23:59:00 +0200

    cleaning code

--- ExcelTools/Pages/ExportTool.xaml.cs
+++ ExcelTools/Pages/ExportTool.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows;
@@ -13,7 +14,6 @@ namespace ExcelTools.Pages
     {
         private Logger Logger { get; }
         private ExcelAnalysis ExcelAnalysis { get; }
-        private ExportSettings Settings { get; }
 
         public ExportTool(Logger logger)
         {
@@ -22,9 +22,9 @@ namespace ExcelTools.Pages
             this.Logger = logger;
 
             this.ExcelAnalysis = new ExcelAnalysis(this.Logger);
-
-            this.Settings = new ExportSettings(this.SettingsContainer);
         }
+
+        private string[] separators = {" ", Environment.NewLine, ","};
 
         private void RunAnalysis(object sender, RoutedEventArgs e)
         {
@@ -40,7 +40,7 @@ namespace ExcelTools.Pages
 
                 var excelWrapper = new ExcelWrapper(filePath);
 
-                var data = this.ExcelAnalysis.ExportTool(excelWrapper, this.ColumnTextBox.Text, this.Settings.SkipEmpty);
+                var data = this.ExcelAnalysis.ExportTool(excelWrapper, this.ColumnTextBox.Text, this.SkipEmpty.IsChecked == true);
                 
                 if (data == null)
                 {
@@ -49,7 +49,14 @@ namespace ExcelTools.Pages
                 }
 
                 data = data.Select(x => x?.Replace("\n", " ")).ToArray();
-                this.OutputTextbox.Text = string.Join(this.Settings.SelectedSeparator, data);
+
+                int separatorIndex = int.Parse(this.SeparatorsPanel.Children.OfType<RadioButton>()
+                    .FirstOrDefault(r => r.IsChecked == true)
+                    ?.DataContext.ToString());
+
+                string separator = this.separators[separatorIndex];
+
+                this.OutputTextbox.Text = string.Join(separator, data);
             }
             else
             {
@@ -72,7 +79,7 @@ namespace ExcelTools.Pages
 
                 var excelWrappers = filePaths.Select(filePath => new ExcelWrapper(filePath)).ToArray();
 
-                var data = this.ExcelAnalysis.ExportTool(excelWrappers, this.ColumnTextBox.Text, this.Settings.SkipEmpty);
+                var data = this.ExcelAnalysis.ExportTool(excelWrappers, this.ColumnTextBox.Text, this.SkipEmpty.IsChecked == true);
                 
                 if (data == null)
                 {
@@ -81,95 +88,14 @@ namespace ExcelTools.Pages
                 }
                 
                 data = data.Select(x => x?.Replace("\n", " ")).ToArray();
-                this.OutputTextbox.Text = string.Join(this.Settings.SelectedSeparator, data);
-            }
-        }
-    }
 
-    public class ExportSettings
-    {
-        private StackPanel Container { get; }
-        public bool SkipEmpty { get; set; }
+                int separatorIndex = int.Parse(this.SeparatorsPanel.Children.OfType<RadioButton>()
+                    .FirstOrDefault(r => r.IsChecked == true)
+                    ?.DataContext.ToString());
 
-        public ExportSettings(StackPanel container)
-        {
-            this.Container = container;
+                string separator = this.separators[separatorIndex];
 
-            var separationContainer = new StackPanel
-            {
-                Orientation = Orientation.Horizontal
-            };
-
-            var separationLabel = new Label
-            {
-                Content = new StackPanel
-                {
-                    Orientation = Orientation.Horizontal,
-                    Children =
-                    {
-                        new TextBlock
-                        {
-                            Text = "Separator"
-                        },
-                        new TextBlock
-                        {
-                            Text = "*The symbol that will be used to split the data rows*",
-                            FontSize = 10,
-                            VerticalAlignment = VerticalAlignment.Bottom,
-                            Margin = new Thickness(5,0,0,0),
-                            Foreground = Brushes.Gray
-                        }
-                    }
-                }
-            };
-
-            this.SetupSeparationSettings(separationContainer);
-
-            var skipEmptyCheckBox = new CheckBox
-            {
-                Content = "Skip empty cells",
-                Margin = new Thickness(5)
-            };
-
-            skipEmptyCheckBox.Click += (sender, args) => { this.SkipEmpty = skipEmptyCheckBox.IsChecked == true; };
-
-            this.Container.Children.Add(separationLabel);
-            this.Container.Children.Add(separationContainer);
-            this.Container.Children.Add(skipEmptyCheckBox);
-        }
-
-        private readonly Dictionary<string, string> separators = new Dictionary<string, string>
-        {
-            { "Space", " "},
-            { "New Line", "\r\n"},
-            { "Comma", ", "}
-        };
-
-        public string SelectedSeparator { get; set; }
-
-        private void SelectSeparator(object sender, RoutedEventArgs e)
-        {
-            var radioButton = (RadioButton)sender;
-
-            this.SelectedSeparator = this.separators[radioButton.Content.ToString()];
-        }
-
-        private void SetupSeparationSettings(StackPanel separatorContainer)
-        {
-            var radioButtonsContainer = separatorContainer;
-
-            foreach (var pair in this.separators)
-            {
-                var radioButton = new RadioButton
-                {
-                    GroupName = "Separators",
-                    Content = pair.Key,
-                    Margin = new Thickness(40, 0, 0, 0),
-                };
-
-                radioButton.Click += this.SelectSeparator;
-
-                radioButtonsContainer.Children.Add(radioButton);
+                this.OutputTextbox.Text = string.Join(separator, data);
             }
         }
     }
