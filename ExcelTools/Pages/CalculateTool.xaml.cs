b7df8e459057ec0c5c9c7d939a75f4ab2fc3b18b
Stoyan <slownessbg@gmail.com>
2021-01-03 23:56:11 +0200

    adding the ui of the new feature calculate tool

---
+++ ExcelTools/Pages/CalculateTool.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using ExcelTools.Attributes;
+
+namespace ExcelTools.Pages
+{
+    [PageInfo(Header = "Calculate Tool")]
+    public partial class CalculateTool : Page
+    {
+        public CalculateTool()
+        {
+            this.InitializeComponent();
+        }
+
+        private void FileSelection_OnFileSelected()
+        {
+
+        }
+    }
+}
