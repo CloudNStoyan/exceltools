e430dc9a4aa3a58704237678f01360d829ff1210
Stoyan <slownessbg@gmail.com>
2021-01-04 23:55:35 +0200

    adding the basics of calculate tool

--- ExcelTools/Pages/CalculateTool.xaml.cs
+++ ExcelTools/Pages/CalculateTool.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 using ExcelTools.Attributes;
 
 namespace ExcelTools.Pages
@@ -14,6 +15,31 @@ namespace ExcelTools.Pages
         private void FileSelection_OnFileSelected()
         {
 
+        }
+
+        private void RunAnalysis(object sender, RoutedEventArgs e)
+        {
+            var operation = (OperationType) this.OperationSelector.SelectedIndex;
+
+            switch (operation)
+            {
+                case OperationType.Divide:
+                    break;
+                case OperationType.Multiply:
+                    break;
+                case OperationType.Add:
+                    break;
+                case OperationType.Subtract:
+                    break;
+            }
+        }
+
+        private enum OperationType
+        {
+            Divide,
+            Multiply,
+            Add,
+            Subtract
         }
     }
 }
