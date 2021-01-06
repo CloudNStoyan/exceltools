2a4cc7eeed1eaf6160e2af246c6fe3d448d26a5c
Stoyan <slownessbg@gmail.com>
2021-01-06 22:06:42 +0200

    making the rest of the calculate tool logic

--- ExcelTools/Pages/CalculateTool.xaml.cs
+++ ExcelTools/Pages/CalculateTool.xaml.cs
@@ -22,36 +22,47 @@ namespace ExcelTools.Pages
         {
             var operation = (OperationType) this.OperationSelector.SelectedIndex;
 
+            var excelWrapper = new ExcelWrapper(this.FileSelection.SelectedFiles[0]);
+
+            int firstColumn = ExcelWrapper.ConvertStringColumnToNumber(this.FirstColumn.Text);
+            int secondColumn = ExcelWrapper.ConvertStringColumnToNumber(this.SecondColumn.Text);
+
+            double[] firstColumnValues = excelWrapper.GetDoubleRows(firstColumn);
+            double[] secondColumnValues = excelWrapper.GetDoubleRows(secondColumn);
+
+            double[] newValues = new double[firstColumnValues.Length > secondColumnValues.Length
+                ? firstColumnValues.Length
+                : secondColumnValues.Length];
+
             switch (operation)
             {
                 case OperationType.Divide:
-                    var excelWrapper = new ExcelWrapper(this.FileSelection.SelectedFiles[0]);
-
-                    int firstColumn = ExcelWrapper.ConvertStringColumnToNumber(this.FirstColumn.Text);
-                    int secondColumn = ExcelWrapper.ConvertStringColumnToNumber(this.SecondColumn.Text);
-
-                    double[] firstColumnValues = excelWrapper.GetDoubleRows(firstColumn);
-                    double[] secondColumnValues = excelWrapper.GetDoubleRows(secondColumn);
-
-                    double[] newValues = new double[firstColumnValues.Length > secondColumnValues.Length
-                        ? firstColumnValues.Length
-                        : secondColumnValues.Length];
-
                     for (int i = 0; i < newValues.Length; i++)
                     {
                         newValues[i] = firstColumnValues[i] / secondColumnValues[i];
                     }
-
-                    this.Output.Text = string.Join("\n", newValues);
-
                     break;
                 case OperationType.Multiply:
+                    for (int i = 0; i < newValues.Length; i++)
+                    {
+                        newValues[i] = firstColumnValues[i] * secondColumnValues[i];
+                    }
                     break;
                 case OperationType.Add:
+                    for (int i = 0; i < newValues.Length; i++)
+                    {
+                        newValues[i] = firstColumnValues[i] + secondColumnValues[i];
+                    }
                     break;
                 case OperationType.Subtract:
+                    for (int i = 0; i < newValues.Length; i++)
+                    {
+                        newValues[i] = firstColumnValues[i] - secondColumnValues[i];
+                    }
                     break;
             }
+
+            this.Output.Text = string.Join("\n", newValues);
         }
 
         private enum OperationType
