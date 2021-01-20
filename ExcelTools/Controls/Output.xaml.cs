b82c79085e39c569711b131ca37569c41fd78638
Stoyan <slownessbg@gmail.com>
2021-01-20 23:59:17 +0200

    adding new control output

---
+++ ExcelTools/Controls/Output.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+
+namespace ExcelTools.Controls
+{
+    public partial class Output : UserControl
+    {
+        public Output()
+        {
+            this.InitializeComponent();
+        }
+
+        private void SaveOutput(object sender, RoutedEventArgs e)
+        {
+            string text = this.OutputTextBox.Text;
+
+            var saveFileDialog = new SaveFileDialog
+            {
+                DefaultExt = ".txt", InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, text);
+            }
+        }
+    }
+}
