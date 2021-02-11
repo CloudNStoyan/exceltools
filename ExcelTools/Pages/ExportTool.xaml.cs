b26786c1f85a44600baa3ff6ec60ea7267d2874e
Stoyan <slownessbg@gmail.com>
2021-02-11 21:29:48 +0200

    removing extel to datagrid feature and optimizing export tool

--- ExcelTools/Pages/ExportTool.xaml.cs
+++ ExcelTools/Pages/ExportTool.xaml.cs
@@ -18,91 +18,70 @@ namespace ExcelTools.Pages
 
         private void RunAnalysis(object sender, RoutedEventArgs e)
         {
+            int separatorIndex = int.Parse(this.SeparatorsPanel.Children.OfType<RadioButton>()
+                .First(r => r.IsChecked == true).DataContext.ToString());
+
+            string separator = this.separators[separatorIndex];
+
             if (this.FileSelection.MultipleFilesChecked == false)
             {
                 string filePath = this.FileSelection.SelectedFile;
 
-                if (!File.Exists(filePath))
-                {
-                    AlertManager.NoFileSelected();
-                    return;
-                }
-
-                var excelWrapper = new ExcelWrapper(filePath);
-
-                string[] columns = this.SpecificColumnCheckBox.IsChecked == true ? this.Columns.GetColumns() : excelWrapper.GetColumns();
-
-                var data = this.Export(excelWrapper, columns, this.SkipEmpty.IsChecked == true);
-                
-                if (data == null)
-                {
-                    AlertManager.Custom($"There is no column {columns} in {excelWrapper.FileName}");
-                    return;
-                }
-
-                data = data.Select(x => x.Select(y => y?.Replace("\n", " ")).ToArray()).ToArray();
-
-                int separatorIndex = int.Parse(this.SeparatorsPanel.Children.OfType<RadioButton>()
-                    .First(r => r.IsChecked == true).DataContext.ToString());
-
-                string separator = this.separators[separatorIndex];
-                
-
-                this.Output.OutputTextBox.Text = string.Join(separator, this.ConvertDataToTable(data));
+                this.ExportAnalysis(new[] {filePath}, separator);
             }
             else
             {
                 string[] filePaths = this.FileSelection.SelectedFiles;
 
-                if (filePaths == null || filePaths.Length == 0)
-                {
-                    AlertManager.NoFileSelected();
-                    return;
-                }
-
-                foreach (string selectedFile in filePaths)
-                {
-                    if (!File.Exists(selectedFile))
-                    {
-                        AlertManager.Custom($"Cannot find {selectedFile}!");
-                        return;
-                    }
-                }
-
-                var excelWrappers = filePaths.Select(filePath => new ExcelWrapper(filePath)).ToArray();
-
-                string[] column = this.Columns.GetColumns();
-
-                var data = this.Export(excelWrappers, column, this.SkipEmpty.IsChecked == true);
-                
-                if (data == null)
-                {
-                    AlertManager.Custom("No data can be exported");
-                    return;
-                }
-
-                data = data.Select(x => x.Select(y => y.Select(z => z.Replace("\n", " ")).ToArray()).ToArray()).ToArray();
-
-                int separatorIndex = int.Parse(this.SeparatorsPanel.Children.OfType<RadioButton>()
-                    .First(r => r.IsChecked == true).DataContext.ToString());
-
-                string separator = this.separators[separatorIndex];
-
-                var output = new List<string>();
-
-                for (int i = 0; i < data.Length; i++)
-                {
-                    if (this.AddFileName.IsChecked == true)
-                    {
-                        output.Add("====");
-                        output.Add(Path.GetFileName(filePaths[i]));
-                    }
-                    
-                    output.Add(string.Join(separator, this.ConvertDataToTable(data[i])));
-                }
-
-                this.Output.OutputTextBox.Text = string.Join("\r\n", output);
+                this.ExportAnalysis(filePaths, separator);
             }
+        }
+
+        private void ExportAnalysis(string[] filePaths, string separator)
+        {
+            if (filePaths == null || filePaths.Length == 0)
+            {
+                AlertManager.NoFileSelected();
+                return;
+            }
+
+            foreach (string selectedFile in filePaths)
+            {
+                if (!File.Exists(selectedFile))
+                {
+                    AlertManager.Custom($"Cannot find {selectedFile}!");
+                    return;
+                }
+            }
+
+            var excelWrappers = filePaths.Select(filePath => new ExcelWrapper(filePath)).ToArray();
+
+            string[] column = this.Columns.GetColumns();
+
+            var data = this.Export(excelWrappers, column, this.SkipEmpty.IsChecked == true);
+
+            if (data == null)
+            {
+                AlertManager.Custom("No data can be exported");
+                return;
+            }
+
+            data = data.Select(x => x.Select(y => y.Select(z => z.Replace("\n", " ")).ToArray()).ToArray()).ToArray();
+
+            var output = new List<string>();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (this.AddFileName.IsChecked == true)
+                {
+                    output.Add("====");
+                    output.Add(Path.GetFileName(filePaths[i]));
+                }
+
+                output.Add(string.Join(separator, this.ConvertDataToTable(data[i])));
+            }
+
+            this.Output.OutputTextBox.Text = string.Join("\r\n", output);
         }
 
         private string[] ConvertDataToTable(string[][] data)
@@ -152,36 +131,6 @@ namespace ExcelTools.Pages
         }
 
         private const string NoValidColumns = "There are no valid columns!";
-
-        private string[][] Export(ExcelWrapper excelWrapper, string[] column, bool skipEmpty = false)
-        {
-            int[] columnNumbers = column.Select(ExcelWrapper.ConvertStringColumnToNumber).ToArray();
-
-            for (var i = 0; i < columnNumbers.Length; i++)
-            {
-                int columnNumber = columnNumbers[i];
-                if (columnNumber == -1)
-                {
-                    AlertManager.Custom($"Column '{column[i]}' is not a valid column, and it will be skipped!");
-                }
-            }
-
-            columnNumbers = columnNumbers.Where(x => x != -1).ToArray();
-
-            if (columnNumbers.Length < 1)
-            {
-                AlertManager.Custom(NoValidColumns);
-                return null;
-            }
-
-
-            string[][] columnData = !skipEmpty
-                ? columnNumbers.Select(excelWrapper.GetStringRows).ToArray()
-                : columnNumbers.Select(excelWrapper.GetStringRows)
-                    .Select(data => data.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray()).ToArray();
-
-            return columnData;
-        }
 
         private string[][][] Export(ExcelWrapper[] excelWrappers, string[] column, bool skipEmpty = false)
         {
