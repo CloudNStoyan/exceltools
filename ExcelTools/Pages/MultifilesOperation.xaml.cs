77591e48c436730728351a1b4a3f3f7e0c5b56d1
Stoyan <slownessbg@gmail.com>
2021-02-16 20:27:10 +0200

    adding the ui of the new feature multifiles operations

---
+++ ExcelTools/Pages/MultifilesOperation.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Controls;
+using ExcelTools.Attributes;
+
+namespace ExcelTools.Pages
+{
+    [PageInfo(Header = "Multifiles Operation")]
+    public partial class MultifilesOperation : Page
+    {
+        private enum Operation
+        {
+            Concat
+        }
+
+        public MultifilesOperation()
+        {
+            this.InitializeComponent();
+
+            this.SetupUI();
+        }
+
+        private void SetupUI()
+        {
+            string[] operations = Enum.GetNames(typeof(Operation));
+
+            foreach (string operation in operations)
+            {
+                this.OperationDropdown.Items.Add(new ComboBoxItem { Content = new TextBlock { Text = operation } });
+            }
+
+            this.OperationDropdown.SelectedIndex = 0;
+        }
+    }
+}
