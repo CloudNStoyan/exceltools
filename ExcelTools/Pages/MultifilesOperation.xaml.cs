e07db8e1a54d683379583ba4af940006873c0c86
Stoyan <slownessbg@gmail.com>
2021-02-17 23:44:56 +0200

    upgrading framework version and making some progress on multi files function

--- ExcelTools/Pages/MultifilesOperation.xaml.cs
+++ ExcelTools/Pages/MultifilesOperation.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using ExcelTools.Attributes;
 
@@ -9,7 +10,8 @@ namespace ExcelTools.Pages
     {
         private enum Operation
         {
-            Concat
+            Concat,
+            Sum
         }
 
         public MultifilesOperation()
@@ -25,10 +27,29 @@ namespace ExcelTools.Pages
 
             foreach (string operation in operations)
             {
-                this.OperationDropdown.Items.Add(new ComboBoxItem { Content = new TextBlock { Text = operation } });
+                Enum.TryParse(operation, out Operation operationEnum);
+
+                this.OperationDropdown.Items.Add(new ComboBoxItem { Content = new TextBlock { Text = operation }, DataContext = operationEnum});
             }
 
             this.OperationDropdown.SelectedIndex = 0;
+        }
+
+        private void RunOperation(object sender, RoutedEventArgs e)
+        {
+            var operation = (Operation) ((ComboBoxItem) this.OperationDropdown.SelectedItem).DataContext;
+
+            switch (operation)
+            {
+                case Operation.Concat:
+                    this.ConcatOperation();
+                    break;
+            }
+        }
+
+        private void ConcatOperation()
+        {
+            this.Output.OutputTextBox.Text = "test";
         }
     }
 }
