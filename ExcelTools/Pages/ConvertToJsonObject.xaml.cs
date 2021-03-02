caea5266e8f99f6a479e2489907324cfa4a35d85
cloudnstoyan <slownessbg@gmail.com>
2021-03-02 18:38:16 +0200

    adding new feature convert to json object

---
+++ ExcelTools/Pages/ConvertToJsonObject.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Linq;
+using System.Windows.Controls;
+using ExcelTools.Attributes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ExcelTools.Pages
+{
+    [PageInfo(Header = "Convert to JSON Object")]
+    public partial class ConvertToJsonObject : Page
+    {
+        public ConvertToJsonObject()
+        {
+            this.InitializeComponent();
+
+            this.FileSelection.FileSelected += this.Convert;
+        }
+
+        private void Convert()
+        {
+            if (!this.FileSelection.FileIsSelected)
+            {
+                AlertManager.NoFileSelected();
+                return;
+            }
+
+            if (!File.Exists(this.FileSelection.SelectedFile))
+            {
+                AlertManager.Custom("The file doesn't exist!");
+                return;
+            }
+
+            var excelWrapper = new ExcelWrapper(this.FileSelection.SelectedFile);
+
+            string[] columns = excelWrapper.GetColumns();
+
+            var jsonProperties = (
+                from column in columns
+                let dataRows = excelWrapper.GetValueRows(ExcelWrapper.ConvertStringColumnToNumber(column))
+                let obj = new JObject()
+                select new JProperty(dataRows[0], dataRows.Skip(1))
+                ).ToList();
+
+            var jObject = new JObject(jsonProperties);
+
+            this.Output.FileName = excelWrapper.FileName.Split('.')[0] + ".json";
+            this.Output.OutputTextBox.Text = JsonConvert.SerializeObject(jObject, Formatting.Indented);
+        }
+    }
+}
