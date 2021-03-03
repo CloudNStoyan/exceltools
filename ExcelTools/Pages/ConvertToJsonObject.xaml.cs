0127c2e34de0c9074ba8976a63926371a8ad615c
cloudnstoyan <slownessbg@gmail.com>
2021-03-03 23:57:00 +0200

    changing the way it converts excels to json objects

--- ExcelTools/Pages/ConvertToJsonObject.xaml.cs
+++ ExcelTools/Pages/ConvertToJsonObject.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Windows.Controls;
 using ExcelTools.Attributes;
@@ -35,17 +36,44 @@ namespace ExcelTools.Pages
 
             string[] columns = excelWrapper.GetColumns();
 
-            var jsonProperties = (
-                from column in columns
-                let dataRows = excelWrapper.GetValueRows(ExcelWrapper.ConvertStringColumnToNumber(column))
-                let obj = new JObject()
-                select new JProperty(dataRows[0], dataRows.Skip(1))
-                ).ToList();
+            string[] propertyNames = columns.Select(column =>
+                excelWrapper.GetValueRows(ExcelWrapper.ConvertStringColumnToNumber(column))[0]).ToArray();
 
-            var jObject = new JObject(jsonProperties);
+            string[][] propertyValues = columns.Select(column =>
+                    excelWrapper.GetValueRows(ExcelWrapper.ConvertStringColumnToNumber(column)).Skip(1).ToArray())
+                .ToArray();
 
+            int jObjectCount = propertyValues.Select(propertyValue => propertyValue.Length).Max();
+
+            var jsonObjects = new List<JObject>();
+
+            for (int i = 0; i < jObjectCount; i++)
+            {
+                var jsonObject = new JObject();
+
+                for (int j = 0; j < propertyNames.Length; j++)
+                {
+                    jsonObject.Add(propertyNames[j],propertyValues[j][i]);
+                }
+
+                jsonObjects.Add(jsonObject);
+            }
+
+
+            //var jsonProperties = (
+            //    from column in columns
+            //    let dataRows = excelWrapper.GetValueRows(ExcelWrapper.ConvertStringColumnToNumber(column))
+            //    let obj = new JObject()
+            //    select new JProperty(dataRows[0], dataRows.Skip(1))
+            //    ).ToList();
+            //
+            //var jObject = new JObject(jsonProperties);
+            //
+            //this.Output.FileName = excelWrapper.FileName.Split('.')[0] + ".json";
+            //this.Output.OutputTextBox.Text = JsonConvert.SerializeObject(jObject, Formatting.Indented);
             this.Output.FileName = excelWrapper.FileName.Split('.')[0] + ".json";
-            this.Output.OutputTextBox.Text = JsonConvert.SerializeObject(jObject, Formatting.Indented);
+            this.Output.OutputTextBox.Text = JsonConvert.SerializeObject(jsonObjects, Formatting.Indented);
+
         }
     }
 }
