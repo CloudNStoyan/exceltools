7632cdc88b58c35dc7ec588aa52e82dac49770c7
cloudnstoyan <slownessbg@gmail.com>
2021-03-04 20:25:35 +0200

    adding choice mechanism to convert to json object

--- ExcelTools/Pages/ConvertToJsonObject.xaml.cs
+++ ExcelTools/Pages/ConvertToJsonObject.xaml.cs
@@ -36,44 +36,51 @@ namespace ExcelTools.Pages
 
             string[] columns = excelWrapper.GetColumns();
 
-            string[] propertyNames = columns.Select(column =>
-                excelWrapper.GetValueRows(ExcelWrapper.ConvertStringColumnToNumber(column))[0]).ToArray();
+            string typeOfJsonConvert = this.TypeOfJsonConversionContainer.Children.OfType<RadioButton>()
+                .First(r => r.IsChecked == true).DataContext.ToString();
 
-            string[][] propertyValues = columns.Select(column =>
-                    excelWrapper.GetValueRows(ExcelWrapper.ConvertStringColumnToNumber(column)).Skip(1).ToArray())
-                .ToArray();
-
-            int jObjectCount = propertyValues.Select(propertyValue => propertyValue.Length).Max();
-
-            var jsonObjects = new List<JObject>();
-
-            for (int i = 0; i < jObjectCount; i++)
+            if (typeOfJsonConvert == "Rows")
             {
-                var jsonObject = new JObject();
+                string[] propertyNames = columns.Select(column =>
+                    excelWrapper.GetValueRows(ExcelWrapper.ConvertStringColumnToNumber(column))[0]).ToArray();
 
-                for (int j = 0; j < propertyNames.Length; j++)
+                string[][] propertyValues = columns.Select(column =>
+                        excelWrapper.GetValueRows(ExcelWrapper.ConvertStringColumnToNumber(column)).Skip(1).ToArray())
+                    .ToArray();
+
+                int jObjectCount = propertyValues.Select(propertyValue => propertyValue.Length).Max();
+
+                var jsonObjects = new List<JObject>();
+
+                for (int i = 0; i < jObjectCount; i++)
                 {
-                    jsonObject.Add(propertyNames[j],propertyValues[j][i]);
+                    var jsonObject = new JObject();
+
+                    for (int j = 0; j < propertyNames.Length; j++)
+                    {
+                        jsonObject.Add(propertyNames[j], propertyValues[j][i]);
+                    }
+
+                    jsonObjects.Add(jsonObject);
                 }
 
-                jsonObjects.Add(jsonObject);
+                this.Output.FileName = excelWrapper.FileName.Split('.')[0] + ".json";
+                this.Output.OutputTextBox.Text = JsonConvert.SerializeObject(jsonObjects, Formatting.Indented);
             }
+            else
+            {
+                var jsonProperties = (
+                    from column in columns
+                    let dataRows = excelWrapper.GetValueRows(ExcelWrapper.ConvertStringColumnToNumber(column))
+                    let obj = new JObject()
+                    select new JProperty(dataRows[0], dataRows.Skip(1))
+                ).ToList();
 
+                var jObject = new JObject(jsonProperties);
 
-            //var jsonProperties = (
-            //    from column in columns
-            //    let dataRows = excelWrapper.GetValueRows(ExcelWrapper.ConvertStringColumnToNumber(column))
-            //    let obj = new JObject()
-            //    select new JProperty(dataRows[0], dataRows.Skip(1))
-            //    ).ToList();
-            //
-            //var jObject = new JObject(jsonProperties);
-            //
-            //this.Output.FileName = excelWrapper.FileName.Split('.')[0] + ".json";
-            //this.Output.OutputTextBox.Text = JsonConvert.SerializeObject(jObject, Formatting.Indented);
-            this.Output.FileName = excelWrapper.FileName.Split('.')[0] + ".json";
-            this.Output.OutputTextBox.Text = JsonConvert.SerializeObject(jsonObjects, Formatting.Indented);
-
+                this.Output.FileName = excelWrapper.FileName.Split('.')[0] + ".json";
+                this.Output.OutputTextBox.Text = JsonConvert.SerializeObject(jObject, Formatting.Indented);
+            }
         }
     }
 }
