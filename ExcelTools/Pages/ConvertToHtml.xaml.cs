8f0a854cd72b2362bc8dcb6a3ded1a17eea35d38
cloudnstoyan <slownessbg@gmail.com>
2021-03-18 23:31:46 +0200

    adding functionality

--- ExcelTools/Pages/ConvertToHtml.xaml.cs
+++ ExcelTools/Pages/ConvertToHtml.xaml.cs
@@ -1,28 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Linq;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ExcelTools.Attributes;
 
 namespace ExcelTools.Pages
 {
-    /// <summary>
-    /// Interaction logic for ConvertToHtml.xaml
-    /// </summary>
+    [PageInfo(Header = "Convert to HTML")]
     public partial class ConvertToHtml : Page
     {
         public ConvertToHtml()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+
+            this.FileSelection.FileSelected += this.FileSelected;
+        }
+
+        private void FileSelected() => this.Convert(this.FileSelection.SelectedFile);
+
+        private void Convert(string filePath)
+        {
+            var excelWrapper = new ExcelWrapper(filePath);
+
+            string[] columns = excelWrapper.GetColumns();
+
+            string typeOfJsonConvert = this.TypeOfHtmlConversionContainer.Children.OfType<RadioButton>()
+                .First(r => r.IsChecked == true).DataContext.ToString();
+
+            string[] propertyNames = columns.Select(column =>
+                excelWrapper.GetValueRows(ExcelWrapper.ConvertStringColumnToNumber(column))[0]).ToArray();
+
+            string[][] propertyValues = columns.Select(column =>
+                    excelWrapper.GetValueRows(ExcelWrapper.ConvertStringColumnToNumber(column)).Skip(1).ToArray())
+                .ToArray();
+
+            string html = propertyNames.Aggregate("<table><tr>", (current, propertyName) => current + ("\r\n<th>" + propertyName + "</th>"));
+
+            html += "</tr>";
+
+            html = propertyValues.Aggregate(html, (current, values) => current + (string.Join("\r\n<tr>", values.Select(x => $"<td>{x}</td>")) + "</tr>"));
+
+            html += "</table>";
+
+            this.Output.OutputTextBox.Text = html;
+            this.Output.FileName = excelWrapper.FileName.Split('.')[0] + ".html";
         }
     }
 }
